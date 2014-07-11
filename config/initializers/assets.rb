e98e825ca0fb1a8d72c81ea2e5a021be2c8bdf71
hu <workboring@gmail.com>
2014-07-12 02:27:57 +0800

    在Show界面显示内容，在Form显示内容

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,5 @@ Rails.application.config.assets.precompile += %w( products.css )
 Rails.application.config.assets.precompile += %w( plugins/ueditor/ueditor.config.js )
 Rails.application.config.assets.precompile += %w( plugins/ueditor/ueditor.all.min.js )
 Rails.application.config.assets.precompile += %w( plugins/ueditor/ueditor.js )
+Rails.application.config.assets.precompile += %w( plugins/ueditor/parse.js )
 
