c705a2c75dc43995657dc699a67e8bfea9b36418
zhenghongchuan <ZhengHongchuan@zhenghongchuandeMacBook-Pro.local>
2014-07-12 17:41:45 +0800

    后台验证字段长度好了

--- app/models/push_message.rb
+++ app/models/push_message.rb
@@ -2,5 +2,6 @@ class PushMessage < ActiveRecord::Base
 
 belongs_to :company
 has_many :push_targets
+validates :message, presence: true, length: { minimum: 1 }, length: { maximum: 80 }
 
 end
--- spec/models/push_message_spec.rb
+++ spec/models/push_message_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe PushMessage, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '验证' do
+    it '推送消息为空的情况' do
+      push_message = PushMessage.new
+      push_message.message = ""
+      expect(push_message.save).to eq(false)
+    end
+
+    it '推送消息超过80字的情况' do
+      push_message = PushMessage.new
+      push_message.message = "QWERQWERQWERQWERQWERQWERQWRQWRQWRQWRQWRQWERQWREQWREQWREQWRQWRQWREQRWQWRQWRQWRQWRQWREQWERQWERQWREQWREQRE"
+      expect(push_message.save).to eq(false)
+    end
+
+    it '推送消息字数在1到80字的情况' do
+      push_message = PushMessage.new
+      push_message.message = "啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊"
+      expect(push_message.save).to eq(true)
+    end
+  end
+
 end
