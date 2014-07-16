0cd16fa02be4549d94b32d389afc77188b61847c
hu <workboring@gmail.com>
2014-07-16 18:30:23 +0800

    集成了所有css 和 js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,12 +1,4 @@
-Rails.application.config.assets.precompile += ["home.css"]
-Rails.application.config.assets.precompile += %w( sessions.css )
-Rails.application.config.assets.precompile += %w( companies.css )
-Rails.application.config.assets.precompile += %w( informs.css )
-Rails.application.config.assets.precompile += %w( products.css )
-Rails.application.config.assets.precompile += %w( push_messages.css )
-Rails.application.config.assets.precompile += %w( pads.css )
 Rails.application.config.assets.precompile += %w( plugins/ueditor/ueditor.config.js )
 Rails.application.config.assets.precompile += %w( plugins/ueditor/ueditor.all.min.js )
 Rails.application.config.assets.precompile += %w( plugins/ueditor/ueditor.js )
 Rails.application.config.assets.precompile += %w( plugins/ueditor/parse.js )
-
