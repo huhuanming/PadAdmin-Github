314a195c40b1297e464f180e3c1e41417ed9b787
hu <workboring@gmail.com>
2014-07-19 00:15:49 +0800

    合并push message 分支

--- db/schema.rb
+++ db/schema.rb
@@ -56,10 +56,16 @@ ActiveRecord::Schema.define(version: 20140718131240) do
     t.text     "context"
     t.datetime "created_at"
     t.datetime "updated_at"
-<<<<<<< HEAD
     t.string   "brief"
-=======
->>>>>>> origin/PushMessage
+  end
+
+  create_table "news", force: true do |t|
+    t.integer  "company_id"
+    t.string   "title"
+    t.string   "author"
+    t.text     "context"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "pad_users", force: true do |t|
