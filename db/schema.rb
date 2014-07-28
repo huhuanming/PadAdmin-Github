9c73555e4fe07701ae00e774055f23e166c4d295
hu <workboring@gmail.com>
2014-07-28 10:22:06 +0800

    修改了产品不能提交内容的错误

--- db/schema.rb
+++ db/schema.rb
@@ -83,6 +83,15 @@ ActiveRecord::Schema.define(version: 20140725112824) do
     t.datetime "updated_at"
   end
 
+  create_table "news", force: true do |t|
+    t.integer  "company_id"
+    t.string   "title"
+    t.string   "author"
+    t.text     "context"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "pad_users", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
